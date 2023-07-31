17f8c472bd39b6d7290f2722c533610b581488ec
hanzhao2020 <hazhao@eng.ucsd.edu>
2023-07-31 16:17:21 -0700

    Updating PEGASUS wing thickness optimization.

--- demos_om/thickness_opt/pegasus/pegasus_var_th_opt_wint.py
+++ demos_om/thickness_opt/pegasus/pegasus_var_th_opt_wint.py
@@ -5,109 +5,127 @@ from igakit.cad import *
 from igakit.io import VTK
 from GOLDFISH.nonmatching_opt_om import *
 
-class ThicknessOptGroup(om.Group):
+class VarThOptGroup(om.Group):
 
     def initialize(self):
-        self.options.declare('nonmatching_opt')
-        self.options.declare('h_th_name_design', default='thickness')
-        self.options.declare('h_th_name_full', default='thickness_full')
+        self.options.declare('nonmatching_opt_ffd')
+        self.options.declare('h_th_ffd_name', default='thickness')
+        self.options.declare('h_th_fe_name', default='thickness_FE')
+        self.options.declare('h_th_iga_name', default='thickness_IGA')
         self.options.declare('disp_name', default='displacements')
-        self.options.declare('int_energy_name', default='int_E')
+        self.options.declare('int_energy_name', default='w_int')
+        self.options.declare('h_th_ffd_align_name', default='thickness_FFD_align')
         self.options.declare('volume_name', default='volume')
 
     def init_paramters(self):
-        self.nonmatching_opt = self.options['nonmatching_opt']
-        self.h_th_name_design = self.options['h_th_name_design']
-        self.h_th_name_full = self.options['h_th_name_full']
+        self.nonmatching_opt_ffd = self.options['nonmatching_opt_ffd']
+        self.h_th_ffd_name = self.options['h_th_ffd_name']
+        self.h_th_fe_name = self.options['h_th_fe_name']
+        self.h_th_iga_name = self.options['h_th_iga_name']
         self.disp_name = self.options['disp_name']
-        self.int_energy_name = self.options['int_energy_name']
         self.volume_name = self.options['volume_name']
+        self.h_th_ffd_align_name = self.options['h_th_ffd_align_name']
+        self.int_energy_name = self.options['int_energy_name']
 
         self.design_var_lower = 1.0e-3
         self.design_var_upper = 1.0e-1
 
-        self.num_splines = self.nonmatching_opt.num_splines
-        self.init_h_th = [np.average(h_th_sub_array) for h_th_sub_array
-                          in self.nonmatching_opt.init_h_th_list]
+        self.num_splines = self.nonmatching_opt_ffd.num_splines
+        self.nonmatching_opt_ffd.get_init_h_th_multiFFD()
 
         self.inputs_comp_name = 'inputs_comp'
-        self.h_th_map_comp_name = 'h_th_map_comp'
+        self.h_th_ffd2fe_comp_name = 'h_th_ffd2fe_comp'
+        self.h_th_fe2iga_comp_name = 'h_th_fe2iga_comp'
+        self.h_th_ffd_align_comp_name = 'h_th_ffd_align_comp'
         self.disp_states_comp_name = 'disp_states_comp'
-        self.int_energy_comp_name = 'internal_energy_comp'
         self.volume_comp_name = 'volume_comp'
+        self.int_energy_comp_name = 'int_energy_comp'
 
     def setup(self):
         # Add inputs comp
         inputs_comp = om.IndepVarComp()
-        inputs_comp.add_output(self.h_th_name_design,
-                    shape=self.num_splines, val=self.init_h_th)
+        inputs_comp.add_output(self.h_th_ffd_name,
+                    shape=self.nonmatching_opt_ffd.thopt_cpffd_design_size, 
+                    val=self.nonmatching_opt_ffd.init_h_th_multiffd)
         self.add_subsystem(self.inputs_comp_name, inputs_comp)
 
-        # Add h_th map comp
-        self.h_th_map_comp = HthMapComp(
-                        nonmatching_opt=self.nonmatching_opt,
-                        input_h_th_name_design=self.h_th_name_design,
-                        output_h_th_name_full=self.h_th_name_full)
-        self.h_th_map_comp.init_paramters()
-        self.add_subsystem(self.h_th_map_comp_name, self.h_th_map_comp)
+        # Add h_th FFD2FE comp
+        self.h_th_ffd2fe_comp = HthFFD2FEComp(
+                        nonmatching_opt_ffd=self.nonmatching_opt_ffd,
+                        input_h_th_ffd_name=self.h_th_ffd_name,
+                        output_h_th_fe_name=self.h_th_fe_name)
+        self.h_th_ffd2fe_comp.init_paramters()
+        self.add_subsystem(self.h_th_ffd2fe_comp_name, self.h_th_ffd2fe_comp)
+
+        # Add h_th FE2IGA comp
+        self.h_th_fe2iga_comp = HthFE2IGAComp(
+                        nonmatching_opt=self.nonmatching_opt_ffd,
+                        input_h_th_fe_name=self.h_th_fe_name,
+                        output_h_th_iga_name=self.h_th_iga_name)
+        self.h_th_fe2iga_comp.init_paramters()
+        self.add_subsystem(self.h_th_fe2iga_comp_name, self.h_th_fe2iga_comp)
 
         # Add disp_states_comp
         self.disp_states_comp = DispStatesComp(
-                           nonmatching_opt=self.nonmatching_opt,
-                           input_h_th_name=self.h_th_name_full,
+                           nonmatching_opt=self.nonmatching_opt_ffd,
+                           input_h_th_name=self.h_th_iga_name,
                            output_u_name=self.disp_name)
         self.disp_states_comp.init_paramters(save_files=True)
         self.add_subsystem(self.disp_states_comp_name, self.disp_states_comp)
 
         # Add internal energy comp (objective function)
         self.int_energy_comp = IntEnergyComp(
-                          nonmatching_opt=self.nonmatching_opt,
-                          input_h_th_name=self.h_th_name_full,
+                          nonmatching_opt=self.nonmatching_opt_ffd,
+                          input_h_th_name=self.h_th_iga_name,
                           input_u_name=self.disp_name,
                           output_wint_name=self.int_energy_name)
         self.int_energy_comp.init_paramters()
         self.add_subsystem(self.int_energy_comp_name, self.int_energy_comp)
 
-        # Add volume comp (constraint)
+        # Add volume comp (objective function)
         self.volume_comp = VolumeComp(
-                           nonmatching_opt=self.nonmatching_opt,
-                           input_h_th_name=self.h_th_name_full,
+                           nonmatching_opt=self.nonmatching_opt_ffd,
+                           input_h_th_name=self.h_th_iga_name,
                            output_vol_name=self.volume_name)
         self.volume_comp.init_paramters()
         self.add_subsystem(self.volume_comp_name, self.volume_comp)
         self.vol_val = 0
-        for s_ind in range(self.nonmatching_opt.num_splines):
-            self.vol_val += assemble(self.nonmatching_opt.h_th[s_ind]
-                            *self.nonmatching_opt.splines[s_ind].dx)
+        for s_ind in range(self.nonmatching_opt_ffd.num_splines):
+            self.vol_val += assemble(self.nonmatching_opt_ffd.h_th[s_ind]
+                            *self.nonmatching_opt_ffd.splines[s_ind].dx)
 
         # Connect names between components
-
         self.connect(self.inputs_comp_name+'.'
-                     +self.h_th_name_design,
-                     self.h_th_map_comp_name+'.'
-                     +self.h_th_name_design)
+                     +self.h_th_ffd_name,
+                     self.h_th_ffd2fe_comp_name+'.'
+                     +self.h_th_ffd_name)
 
-        self.connect(self.h_th_map_comp_name+'.'
-                     +self.h_th_name_full,
+        self.connect(self.h_th_ffd2fe_comp_name+'.'
+                     +self.h_th_fe_name,
+                     self.h_th_fe2iga_comp_name+'.'
+                     +self.h_th_fe_name)
+
+        self.connect(self.h_th_fe2iga_comp_name+'.'
+                     +self.h_th_iga_name,
                      self.disp_states_comp_name+'.'
-                     +self.h_th_name_full)
+                     +self.h_th_iga_name)
 
-        self.connect(self.h_th_map_comp_name+'.'
-                     +self.h_th_name_full,
-                     self.int_energy_comp_name+'.'
-                     +self.h_th_name_full)
-
-        self.connect(self.h_th_map_comp_name+'.'
-                     +self.h_th_name_full,
+        self.connect(self.h_th_fe2iga_comp_name+'.'
+                     +self.h_th_iga_name,
                      self.volume_comp_name+'.'
-                     +self.h_th_name_full)
+                     +self.h_th_iga_name)
+
+        self.connect(self.h_th_fe2iga_comp_name+'.'
+                     +self.h_th_iga_name,
+                     self.int_energy_comp_name+'.'
+                     +self.h_th_iga_name)
 
         self.connect(self.disp_states_comp_name+'.'+self.disp_name,
                      self.int_energy_comp_name+'.'+self.disp_name)
 
         # Add design variable, constraints and objective
         self.add_design_var(self.inputs_comp_name+'.'
-                            +self.h_th_name_design,
+                            +self.h_th_ffd_name,
                             lower=self.design_var_lower,
                             upper=self.design_var_upper,
                             scaler=1e1)
@@ -161,7 +179,7 @@ folder_name = "results/"
 geom_scale = 2.54e-5  # Convert current length unit to m
 E = Constant(68e9)  # Young's modulus, Pa
 nu = Constant(0.35)  # Poisson's ratio
-h_th = Constant(5.0e-3)  # Thickness of surfaces, m
+# h_th = Constant(3.0e-3)  # Thickness of surfaces, m
 
 p = 3  # spline order
 penalty_coefficient = 1.0e3
@@ -172,12 +190,13 @@ igs_shapes = read_igs_file(filename_igs, as_compound=False)
 pegasus_surfaces = [topoface2surface(face, BSpline=True) 
                   for face in igs_shapes]
 
-# Upper skins: 0, 4, 8, 12, ..., 64
-# Lower skins: 1, 5, 9, 13, ..., 65
-# Front spars: 2, 6, 19, 14, ..., 66
-# Rear spars: 3, 7, 11, 15, ..., 67
-# Ribs: 68, 69, 70, ..., 85 (18 ribs)
-wing_indices = list(range(0,len(pegasus_surfaces)))
+# Upper skins: 0, 4, 8, 12, ..., 68
+# Lower skins: 1, 5, 9, 13, ..., 69
+# Front spars: 2, 6, 19, 14, ..., 70
+# Rear spars: 3, 7, 11, 15, ..., 71
+# Ribs: 72, 73, 74, ..., 89 (18 ribs)
+num_secs = 18
+wing_indices = list(range(0,num_secs*4)) + list(range(72,72+num_secs))
 wing_surfaces = [pegasus_surfaces[i] for i in wing_indices]
 num_surfs = len(wing_surfaces)
 if mpirank == 0:
@@ -219,12 +238,6 @@ if mpirank == 0:
     print("Total DoFs:", preprocessor.total_DoFs)
     print("Number of intersections:", preprocessor.num_intersections_all)
 
-# # Display B-spline surfaces and intersections using 
-# # PythonOCC build-in 3D viewer.
-# display, start_display, add_menu, add_function_to_menu = init_display()
-# # preprocessor.display_surfaces(display, transparency=0.5, show_bdry=False, save_fig=False)
-# preprocessor.display_intersections(display, color='RED', save_fig=False)
-
 if mpirank == 0:
     print("Creating splines...")
 # Create tIGAr extracted spline instances
@@ -243,44 +256,70 @@ for i in range(num_surfs):
 
 h_th = []
 for i in range(num_surfs):
-    h_th += [Function(splines[i].V_linear)]
+    h_th += [Function(splines[i].V_control)]
     h_th[i].interpolate(Constant(5.0e-3))
 
 # Create non-matching problem
-nonmatching_opt = NonMatchingOpt(splines, E, h_th, nu, opt_shape=False, 
-                                 opt_thickness=True, comm=worldcomm)
-nonmatching_opt.create_mortar_meshes(preprocessor.mortar_nels)
+nonmatching_opt = NonMatchingOptFFD(splines, E, h_th, nu, opt_shape=False, 
+                                 opt_thickness=True, var_thickness=True, 
+                                 comm=worldcomm)
 
-if mpirank == 0:
-    print("Setting up mortar meshes...")
+###############################################################
+print("Creating FFD blocks ...")
+outer_skin_end_ind = num_secs*4
+step = 4
+ffd_p = 2
 
-nonmatching_opt.mortar_meshes_setup(preprocessor.mapping_list, 
-                                    preprocessor.intersections_para_coords, 
-                                    penalty_coefficient)
+ffd0_surf_inds = np.arange(0, outer_skin_end_ind, step)
+ffd1_surf_inds = np.arange(1, outer_skin_end_ind, step)
+ffd2_surf_inds = np.arange(2, outer_skin_end_ind, step)
+ffd3_surf_inds = np.arange(3, outer_skin_end_ind, step)
 
-# Define magnitude of load
-load = Constant(1) # The load should be in the unit of N/m^2
-f1 = as_vector([Constant(0.0), Constant(0.0), load])
+nonmatching_opt.set_thopt_multiFFD_surf_inds([ffd0_surf_inds, ffd1_surf_inds,
+                                              ffd2_surf_inds, ffd3_surf_inds])
+num_ffd_blocks = nonmatching_opt.num_thopt_ffd
+cp_ffd_lims_multiffd = nonmatching_opt.thopt_cpsurf_lims_multiffd
 
-# Distributed downward load
-loads = [f1]*num_surfs
-source_terms = []
-residuals = []
-for i in range(num_surfs):
-    z = nonmatching_opt.splines[i].rationalize(
-        nonmatching_opt.spline_test_funcs[i])
-    source_terms += [inner(loads[i], z)\
-                     *nonmatching_opt.splines[i].dx]
-    residuals += [SVK_residual(nonmatching_opt.splines[i], 
-                  nonmatching_opt.spline_funcs[i], 
-                  nonmatching_opt.spline_test_funcs[i], E, nu, h_th[i], 
-                  source_terms[i])]
-nonmatching_opt.set_residuals(residuals)
+ffd_block_list = []
+ffd_block_num_el0 = [2,6,1]
+for field in [2]:
+    cp_range = cp_ffd_lims_multiffd[0][field][1] - cp_ffd_lims_multiffd[0][field][0]
+    cp_ffd_lims_multiffd[0][field][0] = cp_ffd_lims_multiffd[0][field][0] - 0.1*cp_range
+    cp_ffd_lims_multiffd[0][field][1] = cp_ffd_lims_multiffd[0][field][1] + 0.1*cp_range
+    ffd_block_list += [create_3D_block(ffd_block_num_el0, ffd_p, cp_ffd_lims_multiffd[0])]
+ffd_block_num_el1 = [2,6,1]
+for field in [2]:
+    cp_range = cp_ffd_lims_multiffd[1][field][1] - cp_ffd_lims_multiffd[1][field][0]
+    cp_ffd_lims_multiffd[1][field][0] = cp_ffd_lims_multiffd[1][field][0] - 0.1*cp_range
+    cp_ffd_lims_multiffd[1][field][1] = cp_ffd_lims_multiffd[1][field][1] + 0.1*cp_range
+    ffd_block_list += [create_3D_block(ffd_block_num_el1, ffd_p, cp_ffd_lims_multiffd[1])]
 
+ffd_block_num_el2 = [1,6,2]
+for field in [0]:
+    cp_range = cp_ffd_lims_multiffd[2][field][1] - cp_ffd_lims_multiffd[2][field][0]
+    cp_ffd_lims_multiffd[2][field][0] = cp_ffd_lims_multiffd[2][field][0] - 0.1*cp_range
+    cp_ffd_lims_multiffd[2][field][1] = cp_ffd_lims_multiffd[2][field][1] + 0.1*cp_range
+    ffd_block_list += [create_3D_block(ffd_block_num_el2, ffd_p, cp_ffd_lims_multiffd[2])]
+ffd_block_num_el3 = [1,6,2]
+for field in [0]:
+    cp_range = cp_ffd_lims_multiffd[3][field][1] - cp_ffd_lims_multiffd[3][field][0]
+    cp_ffd_lims_multiffd[3][field][0] = cp_ffd_lims_multiffd[3][field][0] - 0.1*cp_range
+    cp_ffd_lims_multiffd[3][field][1] = cp_ffd_lims_multiffd[3][field][1] + 0.1*cp_range
+    ffd_block_list += [create_3D_block(ffd_block_num_el3, ffd_p, cp_ffd_lims_multiffd[3])]
+
+ffd_knots_list = [ffd_block.knots for ffd_block in ffd_block_list]
+ffd_control_list = [ffd_block.control for ffd_block in ffd_block_list]
+print("Setting multiple FFD blocks ...")
+nonmatching_opt.set_thopt_multiFFD(ffd_knots_list, ffd_control_list)
+###############################################################
+
+print("Creating files ...")
 # Set up optimization
 nonmatching_opt.create_files(save_path=save_path, folder_name=folder_name, 
                              thickness=nonmatching_opt.opt_thickness)
-model = ThicknessOptGroup(nonmatching_opt=nonmatching_opt)
+print("Setting variable thickness optimization group ...")
+model = VarThOptGroup(nonmatching_opt_ffd=nonmatching_opt)
+print("Initializing gropu parameters ...")
 model.init_paramters()
 prob = om.Problem(model=model)
 
@@ -291,21 +330,30 @@ if optimizer.upper() == 'SNOPT':
     prob.driver.opt_settings['Major feasibility tolerance'] = 1e-6
     prob.driver.opt_settings['Major optimality tolerance'] = 1e-4
     prob.driver.opt_settings['Major iterations limit'] = 50000
-    prob.driver.opt_settings['Summary file'] = \
-        './SNOPT_report/SNOPT_summary.out'
-    prob.driver.opt_settings['Print file'] = \
-        './SNOPT_report/SNOPT_print.out'
-    prob.driver.options['debug_print'] = ['objs']
+    prob.driver.opt_settings['Summary file'] = './SNOPT_report/SNOPT_summary.out'
+    prob.driver.opt_settings['Print file'] = './SNOPT_report/SNOPT_print.out'
+    prob.driver.options['debug_print'] = ['objs']#, 'desvars']
     prob.driver.options['print_results'] = True
 elif optimizer.upper() == 'SLSQP':
     prob.driver = om.ScipyOptimizeDriver()
     prob.driver.options['optimizer'] = 'SLSQP'
     prob.driver.options['tol'] = 1e-5
     prob.driver.options['disp'] = True
-    prob.driver.options['debug_print'] = ['objs']
+    prob.driver.options['debug_print'] = ['objs']#, 'desvars']
     prob.driver.options['maxiter'] = 50000
 else:
     raise ValueError("Undefined optimizer: {}".format(optimizer))
+
+save_disp = False
+
+if mpirank == 0:
+    print("Saving results...")
+
+if save_disp:
+    for i in range(nonmatching_opt.num_splines):
+        save_results(splines[i], nonmatching_opt.spline_funcs[i], i, 
+                     save_path=save_path, folder=folder_name, 
+                     save_cpfuncs=True, comm=worldcomm)
 
 # Create a recorder variable
 opt_data_dir = save_path+folder_name+'opt_data/'
@@ -330,7 +378,6 @@ prob.driver.add_recorder(recorder)
 prob.setup()
 prob.run_driver()
 
-if mpirank == 0:
-    for i in range(num_surfs):
-        print("Thickness for patch {:2d}: {:10.6f}".format(i, 
-              nonmatching_opt.h_th[i].vector().get_local()[0]))
+major_iter_inds = model.disp_states_comp.func_eval_major_ind
+np.savez(shopt_data_name, major_iter_ind=major_iter_inds, 
+         ffd_control=None, ffd_knots=None, QoI=0.)
